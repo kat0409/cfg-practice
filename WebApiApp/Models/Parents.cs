16539418c00bc75d4fad4eacc74bad71ba7065eb
kat0409 <kathiana119@gmail.com>
2025-10-01 17:57:33 -0500

    Made parents model

---
+++ WebApiApp/Models/Parents.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiApp.Models
+{
+    public class Parents
+    {
+        public string Name { get; set; }
+        public string Pssn { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ChildSsn { get; set; }
+    }
+}
