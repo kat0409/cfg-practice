961b3797b38dd90d8ff6027350300f61da683e74
kat0409 <kathiana119@gmail.com>
2025-10-01 17:20:38 -0500

    Made employees model

---
+++ WebApiApp/Models/Employees.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiApp.Models
+{
+    public class Employees
+    {
+        public string Name { get; set; }
+        public string Ssn { get; set; }
+        public DateOnly HireDate { get; set; }
+        public enum GL
+        {
+            Infant,
+            Kindergarten,
+            Elementary
+        }
+        public GL GradeLevel { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
