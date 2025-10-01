b153d173d97df847774adb985a5cc1b6f092f976
kat0409 <kathiana119@gmail.com>
2025-10-01 17:49:33 -0500

    Made grades model

---
+++ WebApiApp/Models/Grades.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiApp.Models
+{
+    public class Grades
+    {
+        public string AssignmentName { get; set; }
+        public string StudentSsn { get; set; }
+        public int Grade {  get; set; }
+    }
+}
