87092b3324ed65aef0668f33a563ef28785cfc9c
kat0409 <kathiana119@gmail.com>
2025-10-01 18:06:58 -0500

    MAde students model

---
+++ WebApiApp/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApiApp.Models
+{
+    public class Students
+    {
+        public string Name { get; set; }
+        public string StudentSsn { get; set; }
+        public string TeacherSsn { get; set; }
+        public string Parent1Ssn { get; set; }
+        public string Parent2Ssn { get; set; }
+        public enum GL
+        {
+            Infant,
+            Kindergarten,
+            Elementary
+        }
+        public GL GradeLevel { get; set; }
+        public string GradeSubLevel { get; set; }
+    }
+}
