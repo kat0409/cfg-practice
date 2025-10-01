e9a0f47cb0d708d0c8ebc4d2a24698dc5c6c79fd
kat0409 <kathiana119@gmail.com>
2025-10-01 17:48:22 -0500

    Made the employee schedules model

---
+++ WebApiApp/Models/EmployeeSchedules.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiApp.Models
+{
+    public class EmployeeSchedules
+    {
+        public string Essn { get; set; }
+        public int ShiftID { get; set; }
+        public DateTime AssignedAt { get; set; }
+    }
+}
